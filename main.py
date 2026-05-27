eb77879d827133099843a3fc80813d26a16c5a04
Meagher Buys <MeagherBuys@proton.me>
2026-05-27 10:50:51 +0200

    Upgrade password generator project

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello GitHub")
-print("This is a test for GitHub Actions")
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+
+    for _ in range(length):
+        password += random.choice(characters)
+
+    return password
+
+
+length = int(input("How long should your password be? "))
+password = generate_password(length)
+
+print("Generated Password:", password)
